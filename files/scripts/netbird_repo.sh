3a0da71141ee7176a92ee63c21b39d238a3347d9
briantchinn <67764140+briantchinn@users.noreply.github.com>
2025-11-28 18:29:24 -0500

    Create netbird_repo.sh

---
+++ files/scripts/netbird_repo.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+sudo tee /etc/yum.repos.d/netbird.repo <<EOF
+[netbird]
+name=netbird
+baseurl=https://pkgs.netbird.io/yum/
+enabled=1
+gpgcheck=0
+gpgkey=https://pkgs.netbird.io/yum/repodata/repomd.xml.key
+repo_gpgcheck=1
+EOF
