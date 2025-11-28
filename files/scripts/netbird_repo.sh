7989cf526158bf0b35a1ca2056cc93ec8a3d4a53
briantchinn <67764140+briantchinn@users.noreply.github.com>
2025-11-28 18:51:00 -0500

    Update netbird_repo.sh

--- files/scripts/netbird_repo.sh
+++ files/scripts/netbird_repo.sh
@@ -1,4 +1,10 @@
-#!/usr/bin/bash
+!/usr/bin/env bash
+
+# Tell this script to exit if there are any errors.
+# You should have this in every custom script, to ensure that your completed
+# builds actually ran successfully without any errors!
+set -oue pipefail
+
 sudo tee /etc/yum.repos.d/netbird.repo <<EOF
 [netbird]
 name=netbird
